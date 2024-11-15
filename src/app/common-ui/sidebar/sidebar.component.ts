62b4c6ed982148966fefe84c6406df2aa9084bce
ninja095 <ninja095@gmail.com>
2024-11-15 17:23:54 +0100

    sidebar component finished

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,22 +1,20 @@
 import {Component, inject} from '@angular/core';
-import {SvgIconComponent} from "../svg-icon/svg-icon.component";
 import {AsyncPipe, JsonPipe, NgForOf} from "@angular/common";
 import {SubscriberCardComponent} from "./subscriber-card/subscriber-card.component";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {ProfileService} from "../../data/services/profile.service";
 import {firstValueFrom} from "rxjs";
 import {ImgUrlPipe} from "../../helpers/pipes/img-url.pipe";
+import {SvgIconComponent} from "../svg-icon/svg-icon.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     SvgIconComponent,
-    NgForOf,
     SubscriberCardComponent,
     RouterLink,
     AsyncPipe,
-    JsonPipe,
     ImgUrlPipe,
     RouterLinkActive
   ],
@@ -27,7 +25,7 @@ export class SidebarComponent {
   profileService = inject(ProfileService);
   subscribers$ = this.profileService.getSubscribers();
 
-  me = this.profileService.me
+  me = this.profileService.me;
 
   menuItems = [
     {label: 'Моя страница', icon: 'home', link: 'profile/me'},
@@ -36,6 +34,6 @@ export class SidebarComponent {
   ];
 
   ngOnInit() {
-    firstValueFrom(this.profileService.getMe()).then(r => console.log('profile: ',r))
+    firstValueFrom(this.profileService.getMe())
   }
 }
--- src/app/common-ui/svg-icon/svg-icon.component.ts
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -4,8 +4,13 @@ import {Component, Input} from '@angular/core';
   selector: 'svg[icon]',
   standalone: true,
   imports: [],
-  template: '<svg:use [attr.href]="href"></svg:use>',
-  styles: ['']
+  template: `<svg:use [attr.href]="href"></svg:use>`,
+  styles: [`
+    :host {
+      width: var(--svg-icon-width, 18px);
+      height: var(--svg-icon-height, 18px);
+    }
+  `]
 })
 export class SvgIconComponent {
   @Input() icon = '';
