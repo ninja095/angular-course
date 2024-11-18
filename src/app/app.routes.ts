35fc18cf939c6e05f2b1cb82fd106832c231fc4a
ninja095 <ninja095@gmail.com>
2024-11-18 14:37:48 +0100

    страница профиля

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,16 @@ import {SearchPageComponent} from "./pages/search-page/search-page.component";
 import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
 import {LayoutComponent} from "./common-ui/layout/layout.component";
 import {canActivateAuth} from "./auth/access.guard";
+// import {SettingsPageComponent} from "./pages/settings-page/settings-page.component";
 
 export const routes: Routes = [
   {path: '', component: LayoutComponent, children: [
-      { path: '', component: SearchPageComponent },
-      { path: 'profile', component: ProfilePageComponent },
-    ], canActivate: [canActivateAuth]},
-  { path: 'login', component: LoginPageComponent },
+      {path: '', redirectTo: 'profile/me', pathMatch: 'full'},
+      {path: 'profile/:id', component: ProfilePageComponent},
+      // {path: 'settings', component: SettingsPageComponent},
+      {path: 'search', component: SearchPageComponent},
+    ],
+    canActivate: [canActivateAuth]
+  },
+  {path: 'login', component: LoginPageComponent},
 ];
-
